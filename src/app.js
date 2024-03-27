4988aa1b3535dd2e0eb51797bcc525fa7d0c2a07
Gabriela Martínez <160047355+gabrielamartinezch@users.noreply.github.com>
2024-03-27 20:18:03 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Any string");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let textMap = '<ul class="list-group">';
+  textMap += '<li class="alert alert-primary" role="alert">Map method</li>';
+
+  pronoun.map(iterator => {
+    adj.map(item => {
+      noun.map(element => {
+        textMap += `
+          <li class="list-group-item">
+            ${iterator}${item}${element}.com
+          </li>
+        `;
+      });
+    });
+  });
+
+  textMap += "</ul>";
+  document.querySelector("#map").innerHTML = textMap;
 };
